8446907ba2a214e9c2f0f212cffb33cd27989856
aamironline <aamironline@gmail.com>
2011-12-20 23:25:52 +0530

    Removed core files from the test branch.

--- src/_end.js
+++
@@ -1,9 +0,0 @@
-
-
-    return FrameworkFactory;
-
-})(this);
-
-//$class = framework.classCreator;
-//$interface = framework.interfaceCreator;
-//$property = baseObject
--- src/string.js
+++
@@ -1,4 +0,0 @@
-
-    String.trim = String.trim || function(s) {
-        return s.replace(/^\s\s*/, '').replace(/\s\s*$/, '')
-    };
