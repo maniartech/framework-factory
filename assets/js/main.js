e476a9ad4603bfa823dca7985e8c937a248a4798
Talha Maniar <talha.maniar@maniartech.com>
2013-08-02 23:29:44 +0530

    bootstrap changed...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,32 @@
 
-    $(document).ready(function(){
+     $(document).ready(function(){
 
-        $(window).scroll(function(){
-            if ($(this).scrollTop() > 100) {
-                $('.scrollup').fadeIn();
-            } else {
-                $('.scrollup').fadeOut();
-            }
-        });
+    //     $(window).scroll(function(){
+    //         if ($(this).scrollTop() > 100) {
+    //             $('.scrollup').fadeIn();
+    //         } else {
+    //             $('.scrollup').fadeOut();
+    //         }
+    //     });
 
-        $('.scrollup').click(function(){
-            $("html, body").animate({ scrollTop: 0 }, 600);
-            return false;
-        });
+    //     $('.scrollup').click(function(){
+    //         $("html, body").animate({ scrollTop: 0 }, 600);
+    //         return false;
+    //     });
 
-      $('.navbar .dropdown').hover(function() {
-            $(this).find('.dropdown-menu').first().stop(true, true).delay(50).slideDown();
-                    }, function() {
-                $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
-                });
+    //   $('.navbar .dropdown').hover(function() {
+    //         $(this).find('.dropdown-menu').first().stop(true, true).delay(50).slideDown();
+    //                 }, function() {
+    //             $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
+    //
+    //
+    //          });
 
-        });
+            $('.sub-nav-collapse .nav li a').live('click',function(event) {
+    event.preventDefault();
+    $('html, body').animate({scrollTop: $($(this).attr('href')).offset().top-70}, 500);
+});
+
+
+         });
 
